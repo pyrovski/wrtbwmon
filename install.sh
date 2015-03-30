1103d3d1de3ea993636bb06f2a9b6c1c8cdffb57
Peter Bailey <peter.eldridge.bailey@gmail.com>
2015-03-30 04:37:18 +0000

    link table to /www

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 [ -f ./wrtbwmon ] && cp ./wrtbmon* /usr/sbin/ && exit
 wget -O /usr/sbin/wrtbwmon http://raw.githubusercontent.com/pyrovski/wrtbwmon/master/wrtbwmon.sh
+ln -s /tmp/usage.db /www/
