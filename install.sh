294929a2c9e3aa806fe0bd038c52f4a6a265c8f8
Peter Bailey <peter.eldridge.bailey@gmail.com>
2015-04-01 07:09:37 +0000

    fix install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[ -f ./wrtbwmon ] && cp ./wrtbmon* /usr/sbin/ && exit
+[ -f ./wrtbwmon ] && cp ./wrtbwmon* /usr/sbin/ && exit
 wget -O /usr/sbin/wrtbwmon http://raw.githubusercontent.com/pyrovski/wrtbwmon/master/wrtbwmon.sh && chmod +x /usr/sbin/wrtbwmon
 ln -s /tmp/usage.htm /www/
