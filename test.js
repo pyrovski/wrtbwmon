7ec329b3b21656af54b550b0d7e38f7f707bae7c
Peter Bailey <peter.eldridge.bailey@gmail.com>
2015-04-12 18:25:03 +0000

    plot new data from a specific host

--- test.js
+++ test.js
@@ -1,26 +1,46 @@
-var lastTime=0.0
+var lastTime = new Date(0)
 var data=null
+
+function compareEntries(a, b){
+    return (a[0] >= b[0] ? (a[0] == b[0] ? 0 : 1) : -1)
+}
+
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+	return this[this.length - 1]
+    }
+}
 
 function getData(){
     var xmlhttp = new XMLHttpRequest()
     xmlhttp.onreadystatechange=function()
     {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	{
-	    document.getElementById("data").innerHTML=xmlhttp.responseText
-	    data=JSON.parse(xmlhttp.responseText)
-	    //!@todo sort data on client
+    	if (xmlhttp.readyState==4 && xmlhttp.status==200){
+    	    document.getElementById("data").innerHTML=xmlhttp.responseText
+    	    data=JSON.parse(xmlhttp.responseText)
 	    for(var k in data){
-		if(data.hasOwnProperty(k)){
-		    l = data[k].length
-		    if(l > 1){
-			lastTime = Math.max(data[k][l-2][0], lastTime)
-		    }
-		}
+	    	if(data.hasOwnProperty(k)){
+	    	    l = data[k].length
+	    	    if(l > 1){
+	    		data[k] = data[k].slice(0,l-1).sort(compareEntries)
+	    		for(var i = 0; i < data[k].length; i++)
+	    		    data[k][i][0] = new Date(data[k][i][0] * 1000)
+	    		lastTime = new Date(Math.max(data[k].last()[0], lastTime))
+	    	    } else
+	    		data[k] = [[]]
+	    	}
 	    }
-	}
+	    if(data["192.168.1.139"].length > 1){
+		var chart = new google.visualization.AreaChart(document.getElementById('myChart'));
+		dt = [["time","in","out"]].concat(data["192.168.1.139"]).slice(0,data["192.168.1.139"].length)
+		console.debug(dt)
+		dt = google.visualization.arrayToDataTable(dt)
+		
+		chart.draw(dt)
+	    }
+    	}
     }
-    xmlhttp.open("GET","/cgi-bin/test?t=".concat(lastTime),true)
+    xmlhttp.open("GET","/cgi-bin/test?t=".concat(lastTime.getTime()/1000),true)
     xmlhttp.send()
 }
 
