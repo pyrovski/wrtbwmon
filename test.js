50798d4ac7081d5b4145c19a316a25b60577cfe9
Peter Bailey <peter.eldridge.bailey@gmail.com>
2015-04-12 07:17:26 +0000

    update

--- test.js
+++ test.js
@@ -23,28 +23,22 @@ function getData(){
     xmlhttp.open("GET","/cgi-bin/test?t=".concat(lastTime),true)
     xmlhttp.send()
 }
-var data2 = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-	{
-	    label: "My First dataset",
-	    fillColor: "rgba(220,220,220,0.2)",
-	    strokeColor: "rgba(220,220,220,1)",
-	    pointColor: "rgba(220,220,220,1)",
-	    pointStrokeColor: "#fff",
-	    pointHighlightFill: "#fff",
-	    pointHighlightStroke: "rgba(220,220,220,1)",
-	    data: [65, 59, 80, 81, 56, 55, 40]
-	},
-	{
-	    label: "My Second dataset",
-	    fillColor: "rgba(151,187,205,0.2)",
-	    strokeColor: "rgba(151,187,205,1)",
-	    pointColor: "rgba(151,187,205,1)",
-	    pointStrokeColor: "#fff",
-	    pointHighlightFill: "#fff",
-	    pointHighlightStroke: "rgba(151,187,205,1)",
-	    data: [28, 48, 40, 19, 86, 27, 90]
-	}
-    ]
-};
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+	['Year', 'Sales', 'Expenses'],
+	['2013',  1000,      400],
+	['2014',  1170,      460],
+	['2015',  660,       1120],
+	['2016',  1030,      540]
+    ]);
+
+    var options = {
+	title: 'Company Performance',
+	hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+	vAxis: {minValue: 0}
+    };
+
+    var chart = new google.visualization.AreaChart(document.getElementById('myChart'));
+    chart.draw(data, options);
+}
