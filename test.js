90e4b43b40fbe33088d7ee4334074c86a5efc8dd
Peter Bailey <peter.eldridge.bailey@gmail.com>
2015-04-13 00:59:15 +0000

    fix updates, update todo list

--- test.js
+++ test.js
@@ -23,12 +23,13 @@ function parseData(textData, data){
 		newData[key] = newData[key].slice(0,l-1)
 	    	for(var i = 0; i < newData[key].length; i++)
 	    	    newData[key][i][0] = new Date(newData[key][i][0] * 1000)
-	    } else
-	    	newData[key] = [[]]
-	    if(host in compacted)
-		compacted[host] = compacted[host].concat(newData[key])
-	    else
-		compacted[host] = newData[key]
+	    }
+	    if(l > 1){
+		if(host in compacted)
+		    compacted[host] = compacted[host].concat(newData[key])
+		else
+		    compacted[host] = newData[key]
+	    }
 	}
     }
     if(data == null)
@@ -39,15 +40,18 @@ function parseData(textData, data){
 	if(newData.hasOwnProperty(host)){
 	    if(host in compacted){
 		newData[host] =
-		    newData[host].concat(compacted[host]).sort(compareEntries)
-		lastTime =
-		    new Date(Math.max(compacted[host].last()[0], lastTime))
+		    newData[host].concat(compacted[host].sort(compareEntries))
 		delete compacted[host]
 	    }
 	}
     for(var host in compacted)
 	if(host != undefined && compacted.hasOwnProperty(host))
 	    newData[host] = compacted[host].sort(compareEntries)
+    for(var host in newData)
+	if(newData.hasOwnProperty(host) && newData[host].length > 0){
+	    lastTime =
+		new Date(Math.max(newData[host].last()[0], lastTime))
+	}
     return newData
 }
 
@@ -56,12 +60,20 @@ function getData(){
     xmlhttp.onreadystatechange=function()
     {
     	if (xmlhttp.readyState==4 && xmlhttp.status==200){
-    	    data = parseData(xmlhttp.responseText, data)
-    	    //document.getElementById("data").innerHTML=JSON.stringify(data)
+	    oldLastTime = lastTime
+	    data = parseData(xmlhttp.responseText, data)
+	    if(oldLastTime.getTime() == 0){
+		oldLastTime = Infinity
+		for(var host in data)
+		    if(data.hasOwnProperty(host))
+			oldLastTime = new Date(Math.min(data[host][0][0], oldLastTime))
+	    }
+	    document.getElementById("data").innerHTML="got " + (lastTime - oldLastTime)/1000 + " s of data"
+
 	    if("192.168.1.139" in data && data["192.168.1.139"].length > 1){
 		var chart = new google.visualization.AreaChart(document.getElementById('myChart'));
 		//!@todo format data into bytes/s instead of bytes, plot rectangles instead of trapezoids
-		dt = [["time","in","out"]].concat(data["192.168.1.139"]).slice(0,data["192.168.1.139"].length)
+		dt = [["time","in","out"]].concat(data["192.168.1.139"])
 		dt = google.visualization.arrayToDataTable(dt)
 		
 		chart.draw(dt)
