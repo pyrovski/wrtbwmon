29fb1d4dd4914d00707ab23c7f2827073bdc03de
Peter Bailey <peter.eldridge.bailey@gmail.com>
2016-03-12 20:43:17 -0800

    merge install/mkipk scripts

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
-if [ -n "$DESTDIR" ]; then
-    mkdir -p $DESTDIR
-else
-    DESTDIR="/"
-fi
+do_copy() {
+    local dest perm file
+    for file in $*; do
+	dest=`egrep ".+ $file( |$)" ./fileMap | cut -d':' -f1`
+	mkdir -p $DESTDIR/$dest
+	if [ -n `echo $dest | egrep '/s*bin$'` ]; then
+    	    perm=0744
+	else
+    	    perm=0644
+	fi
+	install -m $perm -t $DESTDIR/$dest/ $file
+    done
+}
 
-for file in $*; do
-    dest=`egrep " $file( |$)" ./fileMap | cut -d':' -f1`
-    mkdir -p $DESTDIR/$dest
-    if [ -n `echo $dest | egrep '/bin$'` ]; then
-    	perm=0744
-    else
-    	perm=0644
-    fi
-    if [ -d "$file" ]; then
-    	install -d -m $perm $file $DESTDIR/$dest
-    else
-    	install -m $perm -t $DESTDIR/$dest/ $file
-    fi
-done
+base=`basename $0`
+if [ "$base" = "mkipk.sh" ]; then
+    DESTDIR=$(mktemp -d)
+    do_copy $*
+    fakeroot -- ipkg-build $DESTDIR
+    rm -rf $DESTDIR
+else
+    DESTDIR=${DESTDIR:-"/"}
+    do_copy $*
+fi
--- mkipk.sh
+++ mkipk.sh
@@ -1,17 +1,1 @@
-tmpdir=$(mktemp -d)
-for file in $*; do
-    dest=`egrep " $file( |$)" ./fileMap | cut -d':' -f1`
-    mkdir -p $tmpdir/$dest
-    if [ -n `echo $dest | egrep '/bin$'` ]; then
-    	perm=0744
-    else
-    	perm=0644
-    fi
-    if [ -d "$file" ]; then
-    	install -d -m $perm $file $tmpdir/$dest
-    else
-    	install -m $perm -t $tmpdir/$dest/ $file
-    fi
-done
-fakeroot -- ipkg-build $tmpdir
-rm -rf "$tmpdir"
+install.sh
